6a7a5eabef6791aca66e6af9610edbfb9f1c84b9
vdumchiviy <brain1999@mail.ru>
2023-03-22 17:46:07 -0400

    Reading from .env added

--- src/run_webserver.py
+++ src/run_webserver.py
@@ -1,6 +1,9 @@
 from fastapi import FastAPI
 from src.api.v1.api import api_router
+from src.config.configuration import settings
 
-app = FastAPI(debug=True)
+
+app = FastAPI(debug=settings.DEBUG)
 
 app.include_router(router=api_router, prefix="")
+
