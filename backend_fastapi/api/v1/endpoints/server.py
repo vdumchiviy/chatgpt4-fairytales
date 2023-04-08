9a4722038aebca7371ba3ccf8843b4d8e407e294
vdumchiviy <brain1999@mail.ru>
2023-04-08 17:04:59 -0400

    request for fairytale added

--- backend_fastapi/api/v1/endpoints/server.py
+++ backend_fastapi/api/v1/endpoints/server.py
@@ -7,4 +7,5 @@ router = APIRouter()
 
 @router.get("/info", response_model=ServerShortInfo)
 async def get_server_short_info() -> ServerShortInfo:
-    return ServerShortInfo(version="1.0", last_date="2023-03-23", code=200, msg="OK")
+    result = ServerShortInfo(version="1.0", last_date="2023-03-23", code=200, msg="OK")
+    return result
