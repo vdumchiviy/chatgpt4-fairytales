ba28806db309b1f73fc2cd7f12f16bde3130b046
vdumchiviy <brain1999@mail.ru>
2023-04-11 12:19:06 -0400

    server info cver to schema

--- backend_flask/api/v1/endpoints/server.py
+++ backend_flask/api/v1/endpoints/server.py
@@ -1,10 +1,13 @@
 from flask import Blueprint, jsonify
-from backend.config.configuration import server_info
+# from backend.config.configuration import server_info
+from backend.server.server import get_server_info
+from backend.schemas.server import ServerShortInfo
 
 server = Blueprint('server', __name__)
 
 
 @server.route("/info")
-def get_server_short_info():
-    result = jsonify(server_info)
+async def get_server_short_info():
+    server_info: ServerShortInfo = get_server_info()
+    result = jsonify(server_info.dict())
     return result
